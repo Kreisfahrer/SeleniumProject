e9bb1eb21c4232f937ee203988ba2bcfabd44e66
Electravenik <drugXP@yandex.ru>
2015-02-23 13:11:11 +0300

    test

---
+++ src/main/java/pages/stat/JQueryUIPage.java
@@ -0,0 +1,12 @@
+package pages.stat;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class JQueryUIPage {
+
+    public static void start() {
+        open("/jqueryui/menu");
+
+    }
+
+}
---
+++ src/test/java/JQueryUITest.java
@@ -0,0 +1,13 @@
+import core.TestBase;
+import org.testng.annotations.Test;
+import static pages.stat.JQueryUIPage.start;
+
+public class JQueryUITest extends TestBase{
+
+    @Test
+    public static void startTest(){
+        start();
+    }
+
+
+}
