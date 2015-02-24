9892c19f872b1df6d2e307f320342932e1d89c2a
VladimirMotuzov <lukindi@mail.ru>
2015-02-24 10:03:15 +0300

    WaitForPageToLoad added

--- src/main/java/helpers/Waiter.java
+++ src/main/java/helpers/Waiter.java
@@ -1,5 +1,21 @@
 package helpers;
 
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Waiter {
+
+    public static void waitForPageToLoad(){
+        (new WebDriverWait(WebDriverRunner.getWebDriver(), 10)).until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver webDriver) {
+                JavascriptExecutor js = (JavascriptExecutor) webDriver;
+                return ((String) js.executeScript("return document.readyState")).equalsIgnoreCase("complete");
+            }
+        });
+    }
 
 }
