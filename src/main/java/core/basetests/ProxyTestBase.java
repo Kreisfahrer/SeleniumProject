d91efe66793039f139781e302ebb5e856763ca92
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-02-24 20:31:54 +0300

    ProxyTestBase class extends TestBase, override setup()

--- src/main/java/core/basetests/ProxyTestBase.java
+++ src/main/java/core/basetests/ProxyTestBase.java
@@ -1,32 +1,28 @@
 package core.basetests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import net.lightbody.bmp.core.har.Har;
 import net.lightbody.bmp.proxy.ProxyServer;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.net.PortProber.findFreePort;
 
-public class ProxyTestBase {
+public class ProxyTestBase extends TestBase{
     protected ProxyServer proxyServer;
     protected Har har;
 
+    @Override
     @BeforeMethod
     public void setup() throws Exception {
         if(!getWebDriver().toString().contains("(null)")) {
             getWebDriver().quit();
         }
-        Configuration.browser = "chrome";
-        Configuration.baseUrl = "http://the-internet.herokuapp.com";
-        Configuration.timeout = 10000;
         proxyServer = new ProxyServer(findFreePort());
         proxyServer.start();
         WebDriverRunner.setProxy(proxyServer.seleniumProxy());
-        open("/");
+        super.setup();
     }
 
 
--- src/main/java/core/basetests/TestBase.java
+++ src/main/java/core/basetests/TestBase.java
@@ -3,14 +3,12 @@ package core.basetests;
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeMethod;
 
-import java.io.IOException;
-
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
 
     @BeforeMethod
-    public void setup() throws IOException {
+    public void setup() throws Exception {
         Configuration.browser = "chrome";
         Configuration.baseUrl = "http://the-internet.herokuapp.com";
         Configuration.timeout = 10000;
