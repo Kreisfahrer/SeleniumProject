4d39ca6b9055085a75fc187627280717e0263aad
VladimirMotuzov <lukindi@mail.ru>
2015-03-03 10:53:46 +0300

    Selenide.Wait() added

--- src/main/java/helpers/Waiter.java
+++ src/main/java/helpers/Waiter.java
@@ -1,15 +1,16 @@
 package helpers;
 
-import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static com.codeborne.selenide.Selenide.Wait;
+
 
 public class Waiter {
 
     public static void waitForPageToLoad(){
-        (new WebDriverWait(WebDriverRunner.getWebDriver(), 10)).until(new ExpectedCondition<Boolean>() {
+        Wait().until(new ExpectedCondition<Boolean>() {
             @Override
             public Boolean apply(WebDriver webDriver) {
                 JavascriptExecutor js = (JavascriptExecutor) webDriver;
@@ -19,7 +20,7 @@ public class Waiter {
     }
 
     public static void waitForJquery(){
-        (new WebDriverWait(WebDriverRunner.getWebDriver(), 10)).until(new ExpectedCondition<Boolean>() {
+        Wait().until(new ExpectedCondition<Boolean>() {
             @Override
             public Boolean apply(WebDriver webDriver) {
                 JavascriptExecutor js = (JavascriptExecutor) webDriver;
